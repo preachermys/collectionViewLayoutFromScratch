2cd35129fdacc02d0ea3ea611b4e469f9975eede
Yulia Milovanova <preachermars@gmail.com>
2020-12-11 16:20:42 +0300

    Сделала пример

---
+++ collectionViewLayoutFromScratch/Cell.swift
@@ -0,0 +1,13 @@
+//
+//  Cell.swift
+//  collectionViewLayoutFromScratch
+//
+//  Created by Preacher on 11.12.2020.
+//  Copyright © 2020 Yulia Milovanova. All rights reserved.
+//
+
+import UIKit
+
+class Cell : UICollectionViewCell {
+    @IBOutlet var lab : UILabel!
+}
